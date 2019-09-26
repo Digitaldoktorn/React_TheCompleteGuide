67c83f61130fd5e5880ccc6dd86c9eff19d576f7
Anders Eriksson <digitaldoktorn@gmail.com>
2019-09-26 19:40:47 +0200

    lecture 1-42 -before hooks

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // DON'T DO LIKE THIS: this.state.persons[0].name = 'Maximillian';
+    this.setState({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 }
+      ]
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a react app</h1>
+        <p>This is working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // the jsx above is the same as following. This is how the code is finally compiled
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
+  }
+}
+
+export default App;
