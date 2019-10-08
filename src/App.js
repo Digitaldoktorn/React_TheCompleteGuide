117b8dc27552913c7d4a1e508cc517589d3b0f9e
Anders Eriksson <digitaldoktorn@gmail.com>
2019-10-08 16:17:25 +0200

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   };
 
   switchNameHandler = () => {
-    // console.log('Was clicked');
+    // console.log('Was clicked') - test;
     // DON'T DO LIKE THIS: this.state.persons[0].name = 'Maximillian';
     this.setState({
       persons: [
