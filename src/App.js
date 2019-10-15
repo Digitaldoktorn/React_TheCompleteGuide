3601f21aa85a550d7359cae8b9960e193049c22d
Anders Eriksson <digitaldoktorn@gmail.com>
2019-10-15 14:37:29 +0200

    module 3 completed

--- src/App.js
+++ src/App.js
@@ -11,23 +11,46 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = newName => {
     // console.log('Was clicked');
     // DON'T DO LIKE THIS: this.state.persons[0].name = 'Maximillian';
     this.setState({
       persons: [
-        { name: "Maximilian", age: 28 },
+        { name: newName, age: 28 },
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 27 }
       ]
     });
   };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 26 }
+      ]
+    });
+  };
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
     return (
       <div className="App">
         <h1>Hi, I am a react app</h1>
         <p>This is working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {/* Bind() is better but using an arrow function as below is also possible: */}
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler("Maximilian!!")}
+        >
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -35,6 +58,8 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!")}
+          changed={this.nameChangedHandler}
         >
           My Hobbies: Racing
         </Person>
